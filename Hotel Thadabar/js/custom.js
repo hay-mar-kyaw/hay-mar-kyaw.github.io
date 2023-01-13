a89b1b687a3b93b1b5775060258de118fb6c35a1
Hay Mar Kyaw <mandalaytvsindiamotorcycles@gmail.com>
2023-01-13 16:57:53 +0630

    File Upload

--- Hotel Thadabar/js/custom.js
+++ Hotel Thadabar/js/custom.js
@@ -26,15 +26,6 @@ $(document).ready(function () {
             }
         }]
     });
-    $('.autoplay').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        dots:false,
-        arrows:false
-      });
-          
     
    
        
